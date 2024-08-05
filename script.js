07cda2b10b5d5d1ab2e2c14cf153ea57f45b55fd
Sujay Trivedi <75556958+sujaytrivedi5@users.noreply.github.com>
2024-08-05 17:46:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
-  // var anim = {alpha:0,blur:10};
-  // Splits text into words and characters
-  const text = new SplitType('#target-one')
+    // let rule = CSSRulePlugin.getRule(".main-heading::before");
 
-  gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin)
-  // gsap code here!
-  gsap.from(".word", { 
-    y: 300,
-    skewX: '-15deg',
-    skewY: '0deg',
-    rotation: 0,
-    perspective: 10000,
-    delay: 1,
-    stagger: 0.2,
-  });
+    // Splits text into words and characters
+    const text = new SplitType('#target-one')
+  
+    gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin, CSSRulePlugin)
+    // gsap code here!
 
-  gsap.to(".main-heading", {
-    alpha: 1,
-    blur: 0,
-    stagger: 0.5,
-    duration: 2,
-  });
+    let t1 = gsap.timeline();
+
+    t1.from(".word", { 
+      y: 300,
+      alpha: 1,
+      skewX: '-15deg',
+      skewY: '0deg',
+      rotation: 0,
+      perspective: 10000,
+      delay: 1,
+      stagger: 0.2,
+    })
+    .from(".text-desc span", {
+      alpha: 0,
+      y: 100,
+      stagger: 0.1,
+    })
+    .from(".button-wrapper", {
+      y: 100,
+      alpha: 0,
+      stagger: 0.5,
+    })
+    // .to(".home-video", {
+    //   scale: 50,
+    //   alpha: 0,
+    //   // stagger: 0.5,
+    // });
 
 });
