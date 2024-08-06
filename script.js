cddd965351aa16d22e752511902527bba2e583a8
Sujay Trivedi <75556958+sujaytrivedi5@users.noreply.github.com>
2024-08-06 16:09:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,80 @@
+// Lenis JS for Smooth Scrolling
+const lenis = new Lenis()
+// lenis.on('scroll', (e) => {
+//   console.log(e)
+// })
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
 document.addEventListener("DOMContentLoaded", (event) => {
+  const lenis = new Lenis()
+  lenis.on('scroll', (e) => {
+    console.log(e)
+  })
 
-    // let rule = CSSRulePlugin.getRule(".main-heading::before");
+  lenis.on('scroll', ScrollTrigger.update)
 
-    // Splits text into words and characters
-    const text = new SplitType('#target-one')
-  
-    gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin, CSSRulePlugin)
-    // gsap code here!
+  gsap.ticker.add((time)=>{
+    lenis.raf(time * 1000)
+  })
 
-    let t1 = gsap.timeline();
+  gsap.ticker.lagSmoothing(0)
 
-    t1.from(".word", { 
-      y: 300,
-      alpha: 1,
-      skewX: '-15deg',
-      skewY: '0deg',
-      rotation: 0,
-      perspective: 10000,
-      delay: 1,
-      stagger: 0.2,
-    })
-    .from(".text-desc span", {
-      alpha: 0,
-      y: 100,
-      stagger: 0.1,
-    })
-    .from(".button-wrapper", {
-      y: 100,
-      alpha: 0,
-      stagger: 0.5,
-    })
-    // .to(".home-video", {
-    //   scale: 50,
-    //   alpha: 0,
-    //   // stagger: 0.5,
-    // });
+  // Splits text into words and characters
+  const text = new SplitType('#target-one')
+  const headingAnimation = new SplitType('.heading-2')
 
+  gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin,CSSRulePlugin)
+  // gsap code here!
+
+  let t1 = gsap.timeline();
+
+  t1.from("#target-one .word", { 
+    y: 300,
+    alpha: 1,
+    skewX: '-15deg',
+    skewY: '0deg',
+    rotation: 0,
+    perspective: 10000,
+    delay: 1,
+    stagger: 0.2,
+  })
+  .from(".text-desc span", {
+    alpha: 0,
+    y: 100,
+    stagger: 0.1,
+  })
+  .from(".button-wrapper", {
+    y: 100,
+    alpha: 0,
+    stagger: 0.5,
+  })
+  .to(".home-video", {
+    // y: -50,
+    alpha: 1,
+    stagger: 0.5,
+  });
+
+  let mm = gsap.matchMedia();
+  mm.add("(min-width: 992px)", () => {
+    gsap.to(".home-video", {
+      y: 50,
+      "--radius1": "10%",
+      scale: 5,
+      scrollTrigger: {
+        trigger: ".video-section",
+        start: "top 50%",
+        end: "+=80",
+        // markers: true,
+        pin: true,
+        scrub: 2,
+        ease: "power2",
+      },
+    });
+  });
 });
